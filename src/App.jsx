2934fafa90c268bf3a59e8c3000629ee978c4d03
ranamiah221 <ranarasul21@gmail.com>
2024-09-16 20:11:51 +0600

    explore jsx props

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,21 @@ function App() {
       
       <h1>Vite + React</h1>
       <Person></Person>
-     
+      <Device name="Laptop" price="45000"></Device>
+      <Device name="Mobile" price="15000"></Device>
+      <Device name="Watch" price="500"></Device>
     </>
   )
   function Person(){
    return(
     <h2>I am Md Rana Miah.</h2>
    )
+  }
+  function Device(prop){
+    console.log(prop);
+    return(
+      <h2>Device name : {prop.name} with Price : {prop.price}</h2>
+    )
   }
 }
 
